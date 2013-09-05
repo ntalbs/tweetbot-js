decd274d72ee2b4d40fd56e8223a5f3603fb3487
ntalbs <ntalbsen@gmail.com>
2013-09-05 14:26:20 +0900

    Restructure the confg.js

--- _config.js
+++ _config.js
@@ -1,14 +1,8 @@
-var config = {
-  oauth_creds: {
-    consumer_key: 'consumer_key',
-    consumer_secret: 'cumsumer_secret',
-    access_token: 'access_token',
-    access_token_secret: 'access_token_secret'
-  },
-  db_uri: 'database-uri',
-  tweet_interval: 1000 * 60 * 60 // one hour
+exports.oauth_creds = {
+  consumer_key: 'consumer_key',
+  consumer_secret: 'consumer_secret',
+  access_token: 'access_token',
+  access_token_secret: 'access-token-secret'
 };
-
-exports.oauth_creds = config.oauth_creds;
-exports.db_uri = config.db_uri;
-exports.tweet_interval = config.tweet_interval;
+exports.db_uri = 'database-uri';
+exports.tweet_interval = 1000 * 60 * 60; // one hour
