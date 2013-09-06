cfc6098ebda6c7073e5a5773959ff1cbe8d8e884
ntalbs <ntalbsen@gmail.com>
2013-09-06 16:54:46 +0900

    Data loader and sample data.

---
+++ loader.js
@@ -0,0 +1,18 @@
+var fs = require('fs'),
+    filename = process.argv[2],
+    mongoose = require('mongoose'),
+    quotesSchema = new mongoose.Schema({msg:String, src:String}, {versionKey:false});
+    quotes = mongoose.model('quotes', quotesSchema);
+
+mongoose.connect('mongodb://tweetbot:kdznbmfsib@paulo.mongohq.com:10098/ntalbs-mongodb');
+
+fs.readFile(filename, 'utf8', function (err, data) {
+  var docs = JSON.parse(data);
+  console.dir(docs);
+
+  quotes.create(docs, function(err) {
+    if (err) throw err;
+  });
+
+  mongoose.disconnect();
+});
