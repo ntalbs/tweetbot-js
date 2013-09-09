f997e90fcc406b06ea91c86e55e479ecd158fb6f
ntalbs <ntalbsen@gmail.com>
2013-09-09 12:08:53 +0900

    Make the json array.

--- backup.js
+++ backup.js
@@ -13,12 +13,14 @@ var escapeQuotes = function (str) {
 out.on('open', function (fd) {
   mongoose.connect(config.db_uri);
   var q_stream = quotes.find().stream();
+  out.write('[\n');
   q_stream.on('data', function (q) {
     out.write('{"msg": "' + escapeQuotes(q.msg) + '", "src": "' + escapeQuotes(q.src) +'"}\n');
   }).on('error', function (err) {
     throw err;
   }).on('close', function () {
     mongoose.disconnect();
+    out.write(']\n');
     out.end();
     console.log('backup completed...');
     console.log('database contents have been written to ' + filename + '.');
