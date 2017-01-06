19d924e5022c8b844b6e377d321adff68f9c9966
ntalbs <ntalbsen@gmail.com>
2017-01-06 23:29:40 +0000

    환경변수 설정 확인

--- config.js
+++ config.js
@@ -1,3 +1,17 @@
+if (!process.env.TWEETBOT_TWITTER_CONSUMER_KEY) {
+  console.error('No twitter consumer key found.')
+  process.exit(1)
+} else if (!process.env.TWEETBOT_TWITTER_CONSUMER_SECRET) {
+  console.error('No twitter consumer secret found.')
+  process.exit(1)
+} else if (!process.env.TWEETBOT_TWITTER_ACCESS_TOKEN) {
+  console.error('No twitter access token found.')
+  process.exit(1)
+} else if (!process.env.TWEETBOT_TWITTER_ACCESS_TOKEN_SECRET) {
+  console.error('No twitter access token secret found.')
+  process.exit(1)
+}
+
 exports.oauth_creds = {
   consumer_key: process.env.TWEETBOT_TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWEETBOT_TWITTER_CONSUMER_SECRET,
